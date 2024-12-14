0f250f356402f7e4f29db83175d55776619ea616
celilbulut <celilbl12@gmail.com>
2024-12-13 21:12:00 -0500

    Aura HUD class

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,15 @@
+// Copy Right Celil Bulut
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+	
+}
