b8c1567b342d52ad85b640ac19fad253f6c6db90
celilbulut <celilbl12@gmail.com>
2024-12-28 20:57:11 -0500

    Initializing Vital Attributes

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -40,4 +40,5 @@ void AAuraCharacterBase::InitializeDefaultAttributes() const
 {
 	ApplyEffectToSelf(DefaultPrimaryAttributes , 1.0f);
 	ApplyEffectToSelf(DefaultSecondaryAttributes , 1.0f);
+	ApplyEffectToSelf(DefaultVitalAttributes , 1.0f);
 }
