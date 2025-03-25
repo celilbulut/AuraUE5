69320c6b1b718c09f3074d127f74d5ea56505694
celilbulut <celilbl12@gmail.com>
2025-03-25 15:45:47 -0400

    Aura Projectile Spell and Attribute UI position

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,17 @@
+// Copy Right Celil Bulut
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo,
+                                           const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Active Ability (C++)"),
+										true, true, FLinearColor::Red, 3);
+}
