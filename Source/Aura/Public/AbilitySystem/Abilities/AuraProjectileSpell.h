fb16642778960bb8b5e2a6636dea2878464bbb86
celilbulut <celilbl12@gmail.com>
2025-03-29 01:11:22 -0400

    Ability Tasks & Sending Gameplay Events

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -17,7 +17,7 @@ class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
 	GENERATED_BODY()
 
 protected:
-
+	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 								 const FGameplayAbilityActorInfo* ActorInfo,
 								 const FGameplayAbilityActivationInfo ActivationInfo,
