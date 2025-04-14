19265980b0d5f527224147fd841f8af7309e566d
celilbulut <celilbl12@gmail.com>
2025-04-13 20:09:07 -0400

    Floating Text Widget

---
+++ Source/Aura/Public/UI/Widgets/DamageTextComponent.h
@@ -0,0 +1,20 @@
+// Copy Right Celil Bulut
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetDamageText(float Damage);
+	
+};
